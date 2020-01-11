a574d9bc79bee8389da54b089abf0033646263a6
I.ermakov <I.ermakov>
2020-01-11 23:07:11 +0300

    1 Post-processing

--- engine/src/commonMain/kotlin/mogot/gl/FrameBuffer.kt
+++ engine/src/commonMain/kotlin/mogot/gl/FrameBuffer.kt
@@ -3,48 +3,30 @@ package mogot.gl
 import mogot.Engine
 import pw.binom.io.Closeable
 
-class FrameBuffer(val engine: Engine,val msaaLevel:MSAALevels) :Closeable{
+class FrameBuffer(val engine: Engine,val widh:Int,val height:Int) :Closeable{
     private var fbo: GLFrameBuffer? = null
-    private val textureTarget = when(msaaLevel){
-        MSAALevels.MSAADisable -> engine.gl.TEXTURE_2D
-        else -> engine.gl.TEXTURE_2D_MULTISAMPLE
-    }
-    enum class MSAALevels(val level:Int){
-        MSAADisable(0),
-        MSAAx2(2),
-        MSAAx4(4),
-        MSAAx8(8),
-        MSAAx16(16)
-    }
-    fun init(widh:Int,height:Int){
+
+    init{
         val gl = engine.gl
         fbo = gl.createFrameBuffer()
     }
     override fun close(){
-        bind()
         fbo?.let {
             engine.gl.deleteBuffer(it)
         }
-        unbind()
     }
     fun bind(){
         val gl = engine.gl
-        gl.enable(textureTarget)
-        if(msaaLevel != MSAALevels.MSAADisable)
-            gl.enable(gl.MULTISAMPLE)
         gl.bindFrameBuffer(gl.FRAMEBUFFER,fbo)
     }
     fun unbind(){
         val gl = engine.gl
-        gl.disable(textureTarget)
-        if(msaaLevel != MSAALevels.MSAADisable)
-            gl.disable(gl.MULTISAMPLE)
         gl.bindFrameBuffer(gl.FRAMEBUFFER,null)
     }
 
-    fun bindTexture(glTexture: GLTexture){
+    fun bindTexture(texture:TextureObject){
         val gl = engine.gl
-        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, textureTarget, glTexture, 0)
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, texture.target, texture.glTexture, 0)
     }
 
     fun check(){
--- engine/src/commonMain/kotlin/mogot/gl/TextureObject.kt
+++ engine/src/commonMain/kotlin/mogot/gl/TextureObject.kt
@@ -3,22 +3,39 @@ package mogot.gl
 import mogot.Engine
 import pw.binom.io.Closeable
 
-class TextureObject(val engine: Engine,multisample:Boolean = false):Closeable {
+class TextureObject(val engine: Engine,val width:Int, val height:Int,val minFilter:FilterParameter = FilterParameter.Linear, magFilter:FilterParameter = FilterParameter.Linear, val multisample: MSAALevels = MSAALevels.MSAADisable):Closeable {
     enum class FilterParameter{
         Nearest,
         Linear
     }
-    val texture:GLTexture = engine.gl.createTexture()
-    private val target = if(multisample) engine.gl.TEXTURE_2D_MULTISAMPLE else engine.gl.TEXTURE_2D
-    fun bind(){
+    enum class MSAALevels(val level:Int){
+        MSAADisable(0),
+        MSAAx2(2),
+        MSAAx4(4),
+        MSAAx8(8),
+        MSAAx16(16)
+    }
+    val glTexture:GLTexture = engine.gl.createTexture()
+    val target = if(multisample == MSAALevels.MSAADisable) engine.gl.TEXTURE_2D_MULTISAMPLE else engine.gl.TEXTURE_2D
+    init {
         engine.gl.enable(target)
-        engine.gl.bindTexture(target,texture)
+        bind()
+        if(multisample == MSAALevels.MSAADisable) {
+            engine.gl.enable(engine.gl.MULTISAMPLE)
+            engine.gl.texImage2DMultisample(engine.gl.TEXTURE_2D_MULTISAMPLE, multisample.level, engine.gl.RGB, width, height, false)
+        }
+        unbind()
+    }
+    fun bind(){
+        engine.gl.bindTexture(target,glTexture)
     }
     fun unbind(){
-
+        engine.gl.bindTexture(target,null)
     }
     override fun close() {
-
+        engine.gl.disable(target)
+        if(multisample != MSAALevels.MSAADisable)
+            engine.gl.disable(engine.gl.MULTISAMPLE)
     }
 
 
