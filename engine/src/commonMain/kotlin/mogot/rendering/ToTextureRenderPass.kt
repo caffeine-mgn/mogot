77fb6586a63c9914a15df14c90aa146ea0b2e3b1
IngarHolsen <I.ermakov>
2020-04-04 21:47:46 +0300

    #37: Move rendering to Common realisation

--- engine/src/commonMain/kotlin/mogot/rendering/ToTextureRenderPass.kt
+++ engine/src/commonMain/kotlin/mogot/rendering/ToTextureRenderPass.kt
@@ -4,13 +4,14 @@ import mogot.gl.GL
 import mogot.gl.RenderTargetTexture
 import mogot.gl.TextureObject
 
-abstract class ToTextureRenderPass(nextPass:RenderPass?) : BaseRenderPass(nextPass) {
+abstract class ToTextureRenderPass(nextPass: RenderPass?) : BaseRenderPass(nextPass) {
     protected var renderTargetTexture: RenderTargetTexture? = null
     override fun setup(context: Display.Context, gl: GL, msaaLevel: TextureObject.MSAALevels) {
         renderTargetTexture?.close()
-        renderTargetTexture = RenderTargetTexture(gl,context.width,context.height,msaaLevel)
+        renderTargetTexture = RenderTargetTexture(gl, context.width, context.height, msaaLevel)
         super.setup(context, gl, msaaLevel)
     }
+
     override fun cleanup() {
         renderTargetTexture?.close()
         super.cleanup()
